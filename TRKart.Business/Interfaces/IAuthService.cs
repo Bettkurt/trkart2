0964ef11aeba30cead746be052d658d51e266b68
bettkurt <betullkk342@gmail.com>
2025-07-24 10:17:57 +0300

    usercar ve transaction eklendi ,frontend iyileştirmeleri

--- TRKart.Business/Interfaces/IAuthService.cs
+++ TRKart.Business/Interfaces/IAuthService.cs
@@ -1,11 +1,10 @@
-﻿using System.Threading.Tasks;
 using TRKart.Entities.DTOs;
 
 namespace TRKart.Business.Interfaces
 {
     public interface IAuthService
     {
-        Task<bool> RegisterAsync(RegisterDto dto);
-        Task<string?> LoginAsync(LoginDto dto);
+        Task<bool> RegisterAsync(RegisterDto registerDto);
+        Task<string?> LoginAsync(LoginDto loginDto);
     }
 }
--- TRKart.DataAccess/SQL kodları/SessionTokens.sql
+++ TRKart.DataAccess/SQL kodları/SessionTokens.sql
@@ -2,7 +2,7 @@ CREATE TABLE "SessionToken" (
     "SessionID" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     "CustomerID" INT NOT NULL,
     "Token" VARCHAR(500) UNIQUE NOT NULL,
-    "Expiration" VARCHAR(20) NOT NULL,
+    "Expiration" TIMESTAMP NOT NULL,
     "CreatedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY ("CustomerID") REFERENCES "Customers"("CustomerID")
 );
